665adab9da956c404d0f6758a11d4468d4e8d1d0
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-06-22 15:26:23 +0700

    exam jp2

---
+++ src/testJP2/Main.java
@@ -0,0 +1,10 @@
+package testJP2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerManage cus = new CustomerManage();
+        cus.run();
+    }
+}
